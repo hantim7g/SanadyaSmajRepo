e36fbe6dd58713015d52d3385e6e0d6c0c740570
Your Name <you@example.com>
2025-06-20 05:46:10 +0530

    First commit

---
+++ SanadyaBrahminSmaj/src/main/java/com/hst/SanadyaBrahminSmajApplication.java
@@ -0,0 +1,13 @@
+package com.hst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanadyaBrahminSmajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanadyaBrahminSmajApplication.class, args);
+	}
+
+}
