bb093f32a4e53cc01f145a6bfa9569769cc9cfdc
Your Name <you@example.com>
2025-06-20 05:58:46 +0530

    test commit

--- SanadyaBrahminSmaj/src/main/java/com/hst/SanadyaBrahminSmajApplication.java
+++ SanadyaBrahminSmaj/src/main/java/com/hst/SanadyaBrahminSmajApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SanadyaBrahminSmajApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SanadyaBrahminSmajApplication.class, args);
 	}
